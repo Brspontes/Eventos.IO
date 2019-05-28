1bfad20f9ed534ac804baacf4e95cfb835968c9e
Brian Pontes <brian.robert16@hotmail.com>
2019-05-27 22:36:17 -0300

    Criando view component, informação de erros em tela, criação da basecontroller

---
+++ src/Eventos.IO.Site/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using Eventos.IO.Domain.Core.NOTIFICATIONS;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eventos.IO.Site.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly IDomainNotificationHandler<DomainNotification> _notifications;
+
+        public BaseController(IDomainNotificationHandler<DomainNotification> notifications)
+        {
+            _notifications = notifications;
+        }
+
+        protected bool OperacaoValida()
+        {
+            return (!_notifications.HasNotifications());
+        }
+    }
+}
---
+++ src/Eventos.IO.Site/ViewComponents/SummaryViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Eventos.IO.Domain.Core.NOTIFICATIONS;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eventos.IO.Site.ViewComponents
+{
+    public class SummaryViewComponent : ViewComponent
+    {
+        private readonly IDomainNotificationHandler<DomainNotification> _notifications;
+
+        public SummaryViewComponent(IDomainNotificationHandler<DomainNotification> notifications)
+        {
+            _notifications = notifications;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //TASK.FROMRESULT = invoca métodos não assincronos para assincrono
+            var notificacoes = await Task.FromResult(_notifications.GetNotifications());
+            notificacoes.ForEach(c => ViewData.ModelState.AddModelError(string.Empty, c.Value));
+            return View();
+        }
+    }
+}
